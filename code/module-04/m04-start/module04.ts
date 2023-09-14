9e52e45d0144677c7fb974aa354418debbe06833
Yael Peet <yaelpeet@gmail.com>
2023-09-14 14:40:31 -0700

    module 4 complete

--- code/module-04/m04-start/module04.ts
+++ code/module-04/m04-start/module04.ts
@@ -7,10 +7,13 @@
 
 /*  TODO: Convert the sortDescending and sortAscending functions to arrow 
     functions. */
-
+// type compareFunctionType = (a:number, b:number) => number;
+interface compareFunctionType {
+    (a:number, b:number): number;
+}
 /*  sortDescending is a comparison function that tells the sort method how to sort 
     numbers in descending order */
-function sortDescending(a, b) {
+let sortDescending : compareFunctionType = (a, b) => {
 if (a > b) {
     return -1;
 } else if (b > a) {
@@ -22,7 +25,7 @@ if (a > b) {
 
 /*  sortDescending is a comparison function that tells the sort method how to sort 
     numbers in ascending order. */
-function sortAscending(a, b) {
+let sortAscending : compareFunctionType = (a, b) => {
 if (a > b) {
     return 1;
 } else if (b > a) {
@@ -38,9 +41,9 @@ if (a > b) {
 
 /*  TODO: Update the BuildArray function. */
 
-function buildArray(items, sortOrder) {
-    let randomNumbers = [];
-    let nextNumber;
+function buildArray(items: number, sortOrder: 'ascending' | 'descending'):number[] {
+    let randomNumbers: number[] = [];
+    let nextNumber: number;
     for (let counter = 0; counter < items; counter++) {
         nextNumber = Math.ceil(Math.random() * (100 - 1));
         if (randomNumbers.indexOf(nextNumber) === -1) {
@@ -58,13 +61,18 @@ function buildArray(items, sortOrder) {
 
 let myArray1 = buildArray(12, 'ascending');
 let myArray2 = buildArray(8, 'descending');
+console.log(myArray1);
+console.log(myArray2);
 
 /*  EXERCISE 2
     TODO: Update the LoanCalculator function. */
 
-function loanCalculator (principle, interestRate, months) {
-    let interest = interestRate / 1200;   // Calculates the monthly interest rate
-    let payment;
+function loanCalculator (principle: number, interestRate: number, months= 12) {
+    let interest: number = interestRate / 1200;   // Calculates the monthly interest rate
+    let payment: number;
     payment = principle * interest / (1 - (Math.pow(1/(1 + interest), months)));
     return payment.toFixed(2);
 }
+
+let myLoan = loanCalculator(1000, 5);
+console.log(myLoan);
