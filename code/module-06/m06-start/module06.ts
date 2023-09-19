e02d3ac0681677b2141a6090b6ab6cbfbd1a79e3
Yael Peet <yaelpeet@gmail.com>
2023-09-19 11:31:01 -0700

    module 6 complete generics

--- code/module-06/m06-start/module06.ts
+++ code/module-06/m06-start/module06.ts
@@ -5,11 +5,11 @@
     Rewrite the DataStore class so the array can store items of any type.
 
     TODO: Add and apply a type variable. */
-class DataStore {
+class DataStore<T> {
 
-    private _data = new Array(10);
+    private _data: Array<T> = new Array(10);
     
-    AddOrUpdate(index: number, item: string) {
+    AddOrUpdate(index: number, item: T) {
         if(index >=0 && index <10) {
             this._data[index] = item;
         } else {
@@ -35,6 +35,19 @@ console.log(cities.GetData(1));         // returns 'Chicago'
 console.log(cities.GetData(12));        // returns 'undefined'
 
 // TODO Test items as numbers.
-
+let empIDS = new DataStore<number>();
+empIDS.AddOrUpdate(0, 50);
+empIDS.AddOrUpdate(1, 65);
+empIDS.AddOrUpdate(2, 89);
+console.log(empIDS.GetData(0));
 
 // TODO Test items as objects.
+type Pets = {
+    name: string;
+    breed: string;
+    age: number
+}
+
+let pets = new DataStore<Pets>();
+pets.AddOrUpdate(0, {name: 'Rex', breed: 'Golden retriever', age: 5});
+console.log(pets.GetData(0));
